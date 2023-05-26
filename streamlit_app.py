b87b43cfb614b1cc4aba6ef1de3bd9034ddec78f
Sivareddie1 <134543139+Sivareddie1@users.noreply.github.com>
2023-05-26 00:22:04 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
-streamlit.title('SUREKHA')
-streamlit.header('HONEY')
+streamlit.title('My Parents New Healthy Dine')
+streamlit.header('Breakfast Menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
